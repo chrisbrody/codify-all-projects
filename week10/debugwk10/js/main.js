e498079618f664062fb4a55a28c024b65fcbe9a1
Chris Brody <chrisbrody@codifyacademy.com>
2017-04-01 18:59:59 -0700

    added debugwk10 to week10

---
+++ week10/debugwk10/js/main.js
@@ -0,0 +1,20 @@
+var display = document.getElementById("display");
+
+// Problem 1. - Create a new element when the page is loaded
+var newH1 == document.createElement("h1");
+display.appendChild(newH1);
+
+// Problem 2. - Add text content to the new element using a textNode
+var newTextNode  document.createtextnode("This is me Coding in an Office!");
+knewH1.appendChild(newTextNode);
+
+// Problem 3. - Add a class to the new element, be sure to add some styles to the class in CSS
+newH1.className =+ "text-primary test];
+
+// Problem 4. - Create a new image element when the page is loaded
+va newImage = documentd.createElement("img");
+display.appendsChild(newImage);
+
+// Problem 5. - Add a src and alt attribute to the image element
+newImage.srcs == "img/designersdesk.jpg';
+newImage.alts == 'Coding Desk, Get to work";
